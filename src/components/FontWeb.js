1cabe86358f3d852d1ee90687756f12137b065c7
Denis <jsherr.6@gmail.com>
2022-02-17 16:31:18 -0500

    cambios

--- src/components/FontWeb.js
+++ src/components/FontWeb.js
@@ -1,4 +1,3 @@
-import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faMagnifyingGlass, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
